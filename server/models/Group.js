c230eb848b2c0d35b6eb51e142e33692cdddf805
Simran kumari <simrangayacs@gmail.com>
2026-07-23 01:21:25 +0530

    Delete server directory

--- server/models/Group.js
+++
@@ -1,6 +0,0 @@
-const groupSchema = new mongoose.Schema({
-  name: String,
-  members: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-  admin: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-});
-module.exports = mongoose.model("Group", groupSchema);

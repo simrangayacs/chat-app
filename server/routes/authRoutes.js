fb424be111557567c971459b7fb311b21282dcef
SIMRAN KUMARI <simrangayacs@gmail.com>
2026-07-22 14:45:45 +0530

    renamed user model to UserModel

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const User = require("../models/user");
+const User = require("../models/UserModel");
 
 // Register
 router.post("/register", async (req, res) => {

a4248d4d291e8fdde5dccafebdda6d8ef093c0fb
SIMRAN KUMARI <simrangayacs@gmail.com>
2026-07-22 22:01:09 +0530

    Updated backend live URL and connection

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ const io = new Server(server, {
   cors: { origin: "http://localhost:3000" }
 });
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
